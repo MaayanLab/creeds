fd0a586da3d49fe8da2aa42924d9396c5ef76732
wangz10@acer <zichen.wang@mssm.edu>
2015-03-04 17:04:43 -0500

    gziped the sqlite db and trying to convert to mysql...

---
+++ clean_genes.py
@@ -0,0 +1,22 @@
+## to clean gene field of entries in GEO collected from microtask1
+import os, sys
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+rcParams['pdf.fonttype'] = 42 ## Output Type 3 (Type3) or Type 42 (TrueType)
+rcParams['font.sans-serif'] = 'Arial'
+
+import urllib2
+import json
+import MySQLdb
+import cPickle as pickle
+from itertools import combinations
+from collections import Counter
+from pprint import pprint
+
+from GEOentry import *
+sys.path.append('C:\Users\Zichen\Documents\\bitbucket\maayanlab_utils')
+from fileIO import file2list, mysqlTable2dict
+sys.path.append('C:\Users\Zichen\Documents\GitHub')
+from clustergram import clustergram
+
