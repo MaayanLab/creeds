de1772ea04b3bd0dcc5ba415078fc02719797a9a
Zichen Wang <wangzc921@gmail.com>
2015-07-02 23:04:58 -0400

    added API endpoints for search signature functionality

--- webapp/app.py
+++ webapp/app.py
@@ -35,6 +35,59 @@ def retrieve_signature():
 			return ('', 400, '')
 
 
+@app.route('/autoCompleteList', methods=['GET', 'POST'])
+@crossdomain(origin='*')
+def get_all_names():
+	if request.method == 'GET':
+		## get a list of names for signatures in mongodb with chdir
+		names = []
+		for uid in ALL_UIDS:
+			sig = DBSignature(uid)
+			if sig.has_chdir():
+				names.append(sig.name)
+		return json.dumps(names)
+
+
+@app.route('/search', methods=['GET', 'POST'])
+@crossdomain(origin='*')
+def search():
+	## handle searching signatures with either custom genes or a document in the db
+	def get_search_results(sig):
+		d_uid_score = sig.calc_all_scores()
+		uid_data = [] # a list of meta data {} sorted by score
+		for uid, score in sorted(d_uid_score.items(), key=lambda x:x[1]): # small to large signed_jaccard
+			projection ={'geo_id':True, 'id':True, '_id':False,
+				'hs_gene_symbol':True, 'disease_name':True, 'drug_name':True}
+			sig_ = DBSignature(uid, projection=projection)
+			meta = sig_.meta
+			meta['name'] = sig_.name
+			meta['signed_jaccard'] = score
+			uid_data.append(meta)
+		return uid_data
+
+	if request.method == 'GET': # search using an id in the mongodb
+		uid = request.args.get('id', '')
+		if uid in ALL_UIDS:
+			sig = DBSignature(uid) # Signature instance
+		else: # invalid uid
+			sig = None
+
+	elif request.method == 'POST': # search using custom up/dn gene list
+		data = json.loads(request.data)
+		up_genes = data['up_genes']
+		dn_genes = data['dn_genes']
+		name = data['name']
+		meta = data['meta']
+		sig = Signature(name, meta, up_genes, dn_genes)
+
+	if sig is not None:
+		uid_data = get_search_results(sig)
+		return json.dumps(uid_data)
+	else:
+		return ('', 400, '')
+
+
+
 if __name__ == '__main__':
 	if len(sys.argv) > 1:
 		port = int(sys.argv[1])
