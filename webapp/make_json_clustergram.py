e154ee0c3fe20ac284ee194e3adf267753c17b6b
Zichen Wang <wangzc921@gmail.com>
2015-07-22 15:41:42 -0400

    added instruction texts on similarity.html

--- webapp/make_json_clustergram.py
+++ webapp/make_json_clustergram.py
@@ -10,10 +10,11 @@ from fileIO import read_df
 def find_name_for_id(uid):
 	projection = {'_id':False, 'limma':False, 'fold_changes':False, 'chdir': False}
 	dbs = DBSignature(uid,projection)
-	return dbs.name
+	return dbs.name + '|' + dbs.meta['geo_id']
 
 # mat, ids, _ = read_df('../signed_jaccard_subset_unique_entries_831x831.txt')
-mat, ids, _ = read_df('../signed_jaccard_subset_unique_entries_519x519.txt')
+# mat, ids, _ = read_df('../signed_jaccard_subset_unique_entries_519x519.txt')
+mat, ids, _ = read_df('../signed_jaccard_subset_unique_entries_259x259.txt')
 
 names = map(find_name_for_id, ids)
 
@@ -21,6 +22,6 @@ json_data = clustergram(mat, names, names,
 	row_linkage='average', col_linkage='average',
 	row_pdist='cosine', col_pdist='cosine')
 
-json.dump(json_data, open('/Library/WebServer/Documents/d3_clustergram/signed_jaccard_subset_clustergram_519.json', 'wb'))	
+json.dump(json_data, open('/Library/WebServer/Documents/d3_clustergram/signed_jaccard_subset_clustergram_259.json', 'wb'))	
 
 
